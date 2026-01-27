2e17debafb7f7c63788702345dab3719c74ccf4d
Julian Koripamo <jkoripamo@gmail.com>
2026-01-27 13:30:42 +0100

    add company type

---
+++ src/types/company.ts
@@ -0,0 +1,12 @@
+export interface CompanySettings {
+  name: string;
+  email?: string;
+  phone?: string;
+  address?: string;
+  city?: string;
+  state?: string;
+  zipCode?: string;
+  logo?: string;
+  taxId?: string;
+  website?: string;
+}
